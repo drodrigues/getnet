0fd392c56dd07ec58d7481bf3f25c743e3e11f79
Danilo Rodrigues <danilo@fillet.com.br>
2021-02-25 14:03:59 -0300

    created the file with getnet's endpoints

---
+++ src/config/Getnet.js
@@ -0,0 +1,13 @@
+const config = {
+  sandbox: {
+    endpoint: 'https://api-sandbox.getnet.com.br',
+  },
+  homolog: {
+    endpoint: 'https://api-homologacao.getnet.com.br',
+  },
+  production: {
+    endpoint: 'https://api.getnet.com.br',
+  },
+};
+
+export default config;
