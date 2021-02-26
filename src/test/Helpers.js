cfd48296242b4e41a5e69d341e3e9994f43dc2be
Danilo Rodrigues <danilo@fillet.com.br>
2021-02-26 10:19:30 -0300

    Created method to mockRequest with fixture on testHelper

--- src/test/Helpers.js
+++ src/test/Helpers.js
@@ -1,4 +1,13 @@
-import { Client } from './../config';
+import MockAdapter from 'axios-mock-adapter';
+import path from 'path';
+import snakeCaseKeys from 'snakecase-keys';
+import fs from 'fs';
+
+import {
+  Client,
+  request,
+  requestAuth,
+} from './../config';
 
 export const setupClient = () => {
   const client = new Client().getInstance();
@@ -7,4 +16,23 @@ export const setupClient = () => {
   client.clientId = process.env.CLIENT_ID;
   client.secret = process.env.CLIENT_SECRET;
   client.env = 'sandbox';
+}
+
+export const readFixture = filename => {
+  const fixture = fs.readFileSync(path.join(__dirname, 'fixtures', filename), 'utf8');
+
+  return fixture;
+}
+
+export const mockRequest = (method, url, status, fixture, data = null) => {
+  const mockAuth = new MockAdapter(requestAuth);
+  const authTokenRequest = readFixture('AuthTokenRequest.json');
+  mockAuth.onPost('/auth/oauth/v2/token').reply(200, authTokenRequest);
+
+  const mock = new MockAdapter(request);
+  if (data) {
+    mock[method](url, snakeCaseKeys(data)).reply(status, readFixture(fixture))
+  } else {
+    mock[method](url).reply(status, readFixture(fixture))
+  }
 }
