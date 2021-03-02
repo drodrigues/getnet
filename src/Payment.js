f8c0580b3da51855dd7cc812a56c82b8ae25c103
Danilo Rodrigues <danilo@fillet.com.br>
2021-03-02 14:47:50 -0300

    Removed console.log

--- src/Payment.js
+++ src/Payment.js
@@ -11,7 +11,6 @@ export default class Payment {
       const { data } = await request.post('/v1/payments/credit', params);
       return data;
     } catch (ex) {
-      console.log(ex.response.data);
       throw handleError(ex);
     }
   }
